150b39d7921aeae2f8211f1ad843d4bf45a0f4ff
calipsomn <calipsomn@yahoo.com>
2016-08-05 19:47:32 +0300

    Deleted

--- Cargo4You/Cargo4You/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Cargo4You.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
