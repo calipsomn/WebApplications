60775b428d627d9dafd3738759f1f5bea133006c
calipsomn <calipsomn@yahoo.com>
2016-08-05 20:17:18 +0300

    Cargo4you

---
+++ Cargo4You/CargoWebApp/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CargoWebApp.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        [Index(IsUnique = true)]
+        [StringLength(255)]
+        public string SessionId { get; set; }
+
+        public virtual ICollection<CartItem> CartItems { get; set; }
+    }
+}
---
+++ Cargo4You/CargoWebApp/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CargoWebApp.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+
+        public virtual Cart Cart { get; set; }
+    }
+}
